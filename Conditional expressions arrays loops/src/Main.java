b80720c84f57a5f16b08ba5bc005a9adaafd79dc
Owner <alina.bilbasova@gmail.com>
2023-03-28 00:12:30 +0200

    Adding a paragraph of text

---
+++ Conditional expressions arrays loops/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] mass= new int [20];
+        mass[0]=1;
+        mass[1]=2;
+        mass[2]=3;
+        mass[3]=4;
+        mass[4]=5;
+        mass[5]=6;
+        mass[6]=7;
+        mass[7]=8;
+        mass[8]=9;
+        mass[9]=10;
+        mass[10]=11;
+        mass[11]=12;
+        mass[12]=13;
+        mass[13]=14;
+        mass[14]=15;
+        mass[15]=16;
+        mass[16]=17;
+        mass[17]=18;
+        mass[18]=19;
+        mass[19]=20;
+        System.out.print(mass[10]);
+
+    }
+}
