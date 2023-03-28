88c92be893a4757faecd9eda8eb7759dcfd28374
Owner <alina.bilbasova@gmail.com>
2023-03-28 13:49:24 +0200

    Adding a paragraph of text

--- Conditional expressions arrays loops/src/Main.java
+++ Conditional expressions arrays loops/src/Main.java
@@ -1,30 +1,18 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-        int [] mass= new int [20];
-        mass[0]=1;
-        mass[1]=2;
-        mass[2]=3;
-        mass[3]=4;
-        mass[4]=5;
-        mass[5]=6;
-        mass[6]=7;
-        mass[7]=8;
-        mass[8]=9;
-        mass[9]=10;
-        mass[10]=11;
-        mass[11]=12;
-        mass[12]=13;
-        mass[13]=14;
-        mass[14]=15;
-        mass[15]=16;
-        mass[16]=17;
-        mass[17]=18;
-        mass[18]=19;
-        mass[19]=20;
-        System.out.print(mass[10]);
-
-
-            }
+        int[] arrayOfNumbers = new int[100];
+        for (int i=0; i<100; i++) {
+            arrayOfNumbers[i] = ThreadLocalRandom.current().nextInt(-100, 100);;
         }
+        int totalSum = 0;
+        for (int numberFromTheArray: arrayOfNumbers) {
+            if (numberFromTheArray > 5 && numberFromTheArray < 45) {
+                totalSum = totalSum + numberFromTheArray;
+            }
+            // TODO: write the if statement printing the number if it's even and positive
+        }
+        System.out.println("Sum of all elements of the array that are >5 and <45: " + totalSum);
     }
 }
